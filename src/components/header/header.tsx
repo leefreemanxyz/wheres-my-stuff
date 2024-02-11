8f647f3f6a7dcd7d19674b059b45caf5a90cf7b5
Lee Freeman <lee@passionatepeople.io>
2024-02-11 14:57:03 +0100

    Update header text and add Card and Collapsible components

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <header className=" flex justify-between items-center h-16 bg-secondary">
-            <h1 className="px-4 text-2xl font-bold text-primary">Where's my stuff?</h1>
+            <h1 className="px-4 text-2xl font-bold text-primary">Where is my stuff?</h1>
         </header>
     )
 }
