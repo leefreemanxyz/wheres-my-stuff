ce408bdd2fa6b36ddfd9b0f5dfb9f294b039591b
Lee Freeman <lee@passionatepeople.io>
2024-02-11 11:05:51 +0100

    Remove default content in page and replace with basic header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className=" flex justify-between items-center h-16 bg-secondary">
+            <h1 className="px-4 text-2xl font-bold text-primary">Where's my stuff?</h1>
+        </header>
+    )
+}
